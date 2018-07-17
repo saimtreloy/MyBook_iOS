b13bae6d188407cc182a1f5bd352bc3c9298898a
saimtreloy <streloy@yahoo.com>
2018-07-17 10:41:00 +0600

    Initial Commit

---
+++ MyBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyBook
+//
+//  Created by Navana Real Estate Ltd. on 17/7/18.
+//  Copyright © 2018 Nano IT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
